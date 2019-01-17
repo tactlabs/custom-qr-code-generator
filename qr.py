88fa56424e4bb62cf451049623bb57d574030db8
Danyson <41253834+Danyson@users.noreply.github.com>
2019-01-17 10:21:33 +0530

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,6 +1,5 @@
 
 import qrcode
-from PIL import Image, ImageDraw
 
 qr = qrcode.QRCode(
     version = 1,
@@ -24,21 +23,5 @@ img = qr.make_image(fill_color="#996000", back_color="black")
 img.save("qr.png")
 
 
-def add_corners(im, rad):
-    circle = Image.new('L', (rad * 2, rad * 2), 0)
-    draw = ImageDraw.Draw(circle)
-    draw.ellipse((0, 0, rad * 2, rad * 2), fill=255)
-    alpha = Image.new('L', im.size, 255)
-    w, h = im.size
-    alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
-    alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
-    alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
-    alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
-    im.putalpha(alpha)
-    return im
-
-im = Image.open('qr.png')
-im = add_corners(im,100)
-im.save('qr.png')
 
 
